f9e71123b01d878bfd9e7a4d400a3d4181d35723
WarenBcs4 <kisumuwebinar@gmail.com>
2025-10-18 22:28:59 +0600

    backend config

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // API Configuration
 const CONFIG = {
-    API_BASE_URL: 'http://localhost:3001/api',
+    API_BASE_URL: 'https://backyard-adventures-backend.vercel.app/api',
     PAYPAL: {
         CLIENT_ID: 'Ad8U43h3NPoniQwgp8rQZdgpHUFuyzuvBXIDN6odr4oLG4wCn2Fo-OsoqKK76ua0iz6xnO-4lKLmRVXk',
         ENVIRONMENT: 'sandbox'
